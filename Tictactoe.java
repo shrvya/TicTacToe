598b155cdaec3c5c8277fa28c18e8c219e3c06e4
unknown <shrivyashetty@gmail.com>
2021-08-26 21:40:22 +0530

    view code

---
+++ Tictactoe.java
@@ -0,0 +1,10 @@
+package tictactoeproject;
+
+public class Tictactoe {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("Welcome to tic tac toe");
+	}
+
+}
