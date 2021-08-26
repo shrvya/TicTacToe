ad375a0d56ae7997b93440635d186f79656c190a
unknown <shrivyashetty@gmail.com>
2021-08-26 22:32:03 +0530

    win or tie

--- Tictactoe.java
+++ Tictactoe.java
@@ -8,8 +8,12 @@ public class Tictactoe {
 	static char[] board = new char[10];
 	static int val=1;
 	static char player,computer;
-	static char input;
+	
 	static int index;
+	static char input,whowin;
+	
+	static boolean toswin,didwin;
+	static int toss,tosschoice,x=0,o=0;
 	static Scanner sc=new Scanner(System.in);
 	static Random random=new Random();
    /*
@@ -94,6 +98,159 @@ public class Tictactoe {
 			return false;
 		}
 	}
+	/*
+	 * if player wins the toss then player plays first
+	 * otherwise computer plays first
+	 */
+	public static void tossing()
+	{
+		System.out.println("Head(1) or Tails(1)");
+		tosschoice=sc.nextInt();
+		toss=random.nextInt(2);
+		if(tosschoice==toss)
+		{
+			System.out.println("Player won the toss.player plays first");
+			toswin=true;
+		}
+		else {
+			System.out.println("computer won the toss,computer plays first");
+			toswin=false;
+		}
+		
+	}
+	/*
+	 * this method will determine which index computer should input to
+	 */
+	public static void computerIndex()
+	{
+		index=random.nextInt(10);
+		if(isEmpty(index)==true)
+		{
+				
+			board[index]=computer;
+			val++;
+			showBoard();	
+		}
+		else {
+			computerIndex();
+		}
+	}
+	/*
+	 * until all index are filled player and computer play alternatively
+	 */
+	public static void startGame()
+	{
+		while(val<10) {
+		if(toswin)
+		{
+			selectIndex();
+			if(val<10)
+			{
+				computerIndex();
+			}
+			
+		}
+		else {
+			computerIndex();
+			if(val<10)
+			{
+				selectIndex();
+			}
+		}
+		}
+		
+	}
+	/*
+	 * this method will check for the winnig condition XXX or OOO
+	 */
+	public static void checkWin()
+	{
+		String line=null;
+		for(int i=1;i<10;i++)
+		{
+			switch(i)
+			{
+			case 1:
+				line = ""+board[1] + board[2] + board[3];
+                break;
+			case 2:
+				line = ""+board[3] + board[4] + board[5];
+                break;
+			case 3:
+				line = ""+board[6] + board[7] + board[8];
+                break;
+			case 4:
+                line = ""+board[1] + board[4] + board[7];
+                break;
+            case 5:
+                line = ""+board[2] + board[5] + board[8];
+                break;
+            case 6:
+                line = ""+board[3] + board[5] + board[9];
+                break;
+            case 7:
+                line = ""+board[1] + board[5] + board[9];
+                break;
+            case 8:
+                line = ""+board[3] + board[5] + board[7];
+                break;
+			}
+			if(line.equals("XXX"))
+			{
+				whowin='X';
+				didwin=true;
+				
+			}
+			else if(line.equals("OOO"))
+			{
+				whowin='O';
+			    didwin=true;
+			    
+			}
+			
+		}
+		
+	}
+	/*
+	 * this method will display the winner
+	 */
+	public static void displayWinner()
+	{
+		
+		checkWin();
+		if(didwin==true) {
+		if(whowin=='X')
+		{
+			System.out.println("Player Won");
+			
+		}
+		else if(whowin=='O'){
+			System.out.println("Computer Won");
+			
+		}
+	}
+		checkTie();
+		}
+	/*
+	 * this method is used to check if there is a tie 
+	 */
+	public static void checkTie()
+	{
+		for(int a=1;a<9;a++)
+		{
+			if(board[a]=='X')
+			{	x++;
+			
+			}else {
+				o++;
+			}
+				
+		}
+		if((x==(o+1)) || (o==(x+1)))
+		{
+			System.out.println("it is a tie");
+		}
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
         System.out.println("Welcome to tic tac toe");
@@ -101,6 +258,10 @@ public class Tictactoe {
         playerInput();
         showBoard();
         selectIndex();
+
+		tossing();
+		startGame();
+		displayWinner();
 	}
 
 }
